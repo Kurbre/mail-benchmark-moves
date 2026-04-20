7df03576707f9ccd8053d5347b1fe1efdc8a8273
Kurbre <catiklou@gmail.com>
2026-04-20 15:37:13 +0300

    Added 1 column table

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  renderMail() {
+    return this.appService.renderMail();
+  }
+
+  @Post(':email')
+  sendMail(@Param('email') email: string) {
+    return this.appService.sendTestMail(email);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Data, renderFile } from 'ejs';
+import juice from 'juice';
+import { join } from 'path';
+import process from 'process';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private transporter;
+
+  constructor(private readonly configService: ConfigService) {
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // true для 465, false для других портов
+      auth: {
+        user: this.configService.getOrThrow<string>('SMTP_USER'),
+        pass: this.configService.getOrThrow<string>('SMTP_PASS'),
+      },
+    });
+  }
+
+  async sendMail(to: string, subject: string, html: string) {
+    return this.transporter.sendMail({
+      from: `"Benchmark moves" <${this.configService.getOrThrow<string>('SMTP_USER')}>`,
+      to,
+      subject,
+      html,
+    });
+  }
+
+  async getTemplate<T extends Data>(templateName: string, data?: T) {
+    const file = join(process.cwd(), 'src', 'mail', `${templateName}.ejs`);
+
+    const template = await renderFile(file, data);
+
+    return juice(template);
+  }
+
+  async renderMail() {
+    return this.getTemplate('benchmarkMoveTable', {});
+  }
+
+  async sendTestMail(email: string) {
+    const template = await this.getTemplate('benchmarkMoveTable', {});
+
+    await this.sendMail(email, 'Benchmark moves', template);
   }
 }
