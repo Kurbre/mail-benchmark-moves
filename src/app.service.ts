e8fdf2511189ae82958245cadbc63656dcfbb5ef
Kurbre <catiklou@gmail.com>
2026-04-20 16:18:43 +0300

    Added data from table

--- src/app.service.ts
+++ src/app.service.ts
@@ -40,7 +40,55 @@ export class AppService {
   }
 
   async renderMail() {
-    return this.getTemplate('benchmarkMoveTable', {});
+    const indexes = [
+      {
+        symbol: 'N225',
+        name: 'Nikkei 225',
+        price: '58,475.90',
+        change: '-1,042.22',
+        percent: '-1.78%',
+        logo: 'https://www.shutterstock.com/image-photo/coca-cola-logo-on-vibrant-600nw-2524254995.jpg',
+        isPositive: false,
+      },
+      {
+        symbol: 'S&P 500',
+        name: "Standard & Poor's",
+        price: '5,123.45',
+        change: '+12.30',
+        percent: '+0.24%',
+        logo: 'https://static.vecteezy.com/system/resources/previews/007/978/618/non_2x/pepsi-popular-drink-brand-logo-vinnytsia-ukraine-may-16-202-free-vector.jpg',
+        isPositive: true,
+      },
+      {
+        symbol: 'N225',
+        name: 'Nikkei 225',
+        price: '58,475.90',
+        change: '-1,042.22',
+        percent: '-1.78%',
+        logo: 'https://www.shutterstock.com/image-photo/coca-cola-logo-on-vibrant-600nw-2524254995.jpg',
+        isPositive: false,
+      },
+      {
+        symbol: 'S&P 500',
+        name: "Standard & Poor's",
+        price: '5,123.45',
+        change: '+12.30',
+        percent: '+0.24%',
+        logo: 'https://static.vecteezy.com/system/resources/previews/007/978/618/non_2x/pepsi-popular-drink-brand-logo-vinnytsia-ukraine-may-16-202-free-vector.jpg',
+        isPositive: true,
+      },
+      {
+        symbol: 'Toyota',
+        name: 'Toyota prius',
+        price: '15,123.45',
+        change: '+32.30',
+        percent: '+2.24%',
+        logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZShoVBhCZoflB-I35i_BsVH9Lexeanbbq2Q&s',
+        isPositive: true,
+      },
+    ];
+
+    return this.getTemplate('benchmarkMoveTable', { indexes });
   }
 
   async sendTestMail(email: string) {
